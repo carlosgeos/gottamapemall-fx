8c79c403fbefaafd934e97d3d385a090a10cd632
Carlos Requena López <crequena@ulb.ac.be>
2017-03-31 14:17:40 +0200

    gradle builds client OK

--- src/main/java/be/ac/ulb/infof307/g07/client/MainGUI.java
+++ src/main/java/be/ac/ulb/infof307/g07/client/MainGUI.java
@@ -1,7 +1,4 @@
-package be.ac.ulb.infof307.g07; 
-/**
- * Created by carlos on 3/11/17.
- */
+package be.ac.ulb.infof307.g07;
 
 import com.lynden.gmapsfx.GoogleMapView;
 import com.lynden.gmapsfx.MapComponentInitializedListener;
@@ -22,61 +19,60 @@ import javafx.stage.Stage;
 /**
  * <b>MainGUI est la classe qui se charge de lancer l'interface graphique (afficher la carte dans la fenetre).</b>
  * Elle hérite de la classe Application de javafx.
- * 
+ *
  * @see MapView
- * 
- * @author carlos
+ *
  * @version 1.0
  */
 public class MainGUI extends Application implements MapComponentInitializedListener{
 
-	
-	private int mainWindowWidth = 800;
-	private int mainWindowHeight = 600;
-	
-	private GoogleMapView googleMapView;
-	private GoogleMap googleMap;
-	private static String apikey = "AIzaSyA38gCIADhL0JWZbNmPYtsTgGJJWIyXZNI";
-	
-	private MapView pokeMapView;
-	
-	@Override
+
+    private int mainWindowWidth = 800;
+    private int mainWindowHeight = 600;
+
+    private GoogleMapView googleMapView;
+    private GoogleMap googleMap;
+    private static String apikey = "AIzaSyA38gCIADhL0JWZbNmPYtsTgGJJWIyXZNI";
+
+    private MapView pokeMapView;
+
+    @Override
     public void start(Stage primaryStage) {
-		
-		
-		googleMapView = new GoogleMapView(null, apikey);
-		googleMapView.addMapInializedListener(this);
-		
-		primaryStage.setWidth(mainWindowWidth);
-		primaryStage.setHeight(mainWindowHeight);
-		primaryStage.setTitle("PokéMap");
-		
-		BorderPane mapBorderPane = new BorderPane();
-		mapBorderPane.setCenter(googleMapView);
-		mapBorderPane.setMinSize(mainWindowWidth, mainWindowHeight);
-		
-		GridPane mainGrid = new GridPane();
-		mainGrid.add( mapBorderPane, 0, 0);
-		
-		Scene mainScene = new Scene(mainGrid);
-		primaryStage.setScene(mainScene);
-		primaryStage.show();
+
+
+	googleMapView = new GoogleMapView(null, apikey);
+	googleMapView.addMapInializedListener(this);
+
+	primaryStage.setWidth(mainWindowWidth);
+	primaryStage.setHeight(mainWindowHeight);
+	primaryStage.setTitle("PokéMap");
+
+	BorderPane mapBorderPane = new BorderPane();
+	mapBorderPane.setCenter(googleMapView);
+	mapBorderPane.setMinSize(mainWindowWidth, mainWindowHeight);
+
+	GridPane mainGrid = new GridPane();
+	mainGrid.add( mapBorderPane, 0, 0);
+
+	Scene mainScene = new Scene(mainGrid);
+	primaryStage.setScene(mainScene);
+	primaryStage.show();
     }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         launch(args);
     }
 
-	@Override
-	public void mapInitialized() {
-		
-		// this method is going to be called when the GoogleMap is totally loaded in GoogleMapView
-		// So before the finish loading we can't do anything else, because the instance GoogleMap can't be created.
-		Map pokeMap = new Map();
-		MapView pokeMapView = new MapView(googleMapView);
-		MapController mapController = new MapController(pokeMap, pokeMapView);
-		
-		
-	}
+    @Override
+    public void mapInitialized() {
+
+	// this method is going to be called when the GoogleMap is totally loaded in GoogleMapView
+	// So before the finish loading we can't do anything else, because the instance GoogleMap can't be created.
+	Map pokeMap = new Map();
+	MapView pokeMapView = new MapView(googleMapView);
+	MapController mapController = new MapController(pokeMap, pokeMapView);
+
+
+    }
 
 }
