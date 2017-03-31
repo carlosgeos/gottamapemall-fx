151c54b5dd69dc3f141b357443c0a036e93345d8
Perale Thomas <thomas.perale@openmailbox.org>
2017-03-31 15:16:56 +0200

    Moved different projects parts in different packages.

--- server/src/main/java/be/ac/ulb/infof307/g07/Main.java
+++ server/src/main/java/be/ac/ulb/infof307/g07/Main.java
@@ -1,6 +1,7 @@
-package be.ac.ulb.infof307.g07.server;
+package be.ac.ulb.infof307.g07;
 
 import static spark.Spark.*;
+
 public class Main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
