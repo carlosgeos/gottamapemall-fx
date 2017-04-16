0ae24302d85c6015ad38302f4cabdecb4a148722
Carlos Requena López <crequena@ulb.ac.be>
2017-04-16 22:18:40 +0200

    javadoc

--- client/src/main/java/be/ac/ulb/infof307/g07/Views/MapView.java
+++ client/src/main/java/be/ac/ulb/infof307/g07/Views/MapView.java
@@ -16,126 +16,125 @@ import be.ac.ulb.infof307.g07.Models.Map;
 import be.ac.ulb.infof307.g07.Models.PokeMarker;
 
 /**
- * 
+ *
  * Cette classe represente la vue dans la structure MVC cad la classe s occupant de l affichage de la carte et de ses composantes graphiques.
- * 
+ *
  * <p>
  * Un objet de cette classe est cree par les classes suivantes: MapController, MapMouseDblClickHandler et MainGUI, dans leurs constructeurs respectifs et la methode mapInitialized() de MainGUI.
  * <p>
- * 
+ *
  * @version 1.0
  *
  * @see be.ac.ulb.infof307.g07.MainGUI
  * @see be.ac.ulb.infof307.g07.Controllers.MapController
  * @see be.ac.ulb.infof307.g07.Controllers.Handlers.MapMouseDblClickHandler
- * @see be.ac.ulb.infof307.g07.MainGUI#mapInitialized
  *
  */
-public class MapView{ 
-	
+public class MapView{
 
-	/**
-	 * Une table de hachage contenant les markers (epingles) presents sur la carte et leurs identifiants (nombre entier servant de clef).
-	 * 
-	 * @see be.ac.ulb.infof307.g07.Models.Map#pokeMarkers
-	 * 
-	 */
-	private HashMap<Integer, Marker> markersOnMap= new HashMap<Integer, Marker>();
-	
-	
-	/**
-	 * Un objet GoogleMap pour afficher la carte google sur base de l'objet GoogleMapView.
-	 * 
-	 * @see MapView#getGoogleMap()
+
+    /**
+     * Une table de hachage contenant les markers (epingles) presents sur la carte et leurs identifiants (nombre entier servant de clef).
+     *
+     * @see be.ac.ulb.infof307.g07.Models.Map#pokeMarkers
+     *
+     */
+    private HashMap<Integer, Marker> markersOnMap= new HashMap<Integer, Marker>();
+
+
+    /**
+     * Un objet GoogleMap pour afficher la carte google sur base de l'objet GoogleMapView.
+     *
+     * @see MapView#getGoogleMap()
      * @see MapView#MapView()
      */
-	private GoogleMap googleMap;
+    private GoogleMap googleMap;
 
 
-	
-	/**
+
+    /**
      * Constructeur de l'objet MapView.
-	 * <p>
-     * On cree on objet MapOptions avec certains parametres predefinis. 
+     * <p>
+     * On cree on objet MapOptions avec certains parametres predefinis.
      * On cree une googleMapView (sur base de ce MapOptions) via la methode createMap et on l'assigne a notre objet googleMap.
      * </p>
-     * 
-     *@param newGoogleMapView
-	 * 						la vue pour la carte googlemaps.
-	 * 
-	 *@see MapView#googleMap
-	 * 
-	 */
-	public MapView( GoogleMapView newGoogleMapView){
-		
-		MapOptions defaultMapOptions = new MapOptions();
-		// here we set the default location as 47.6097, -122.3331
-		LatLong defaultMapCenterPosition = new LatLong(47.6097, -122.3331);
-		
-		defaultMapOptions.center(defaultMapCenterPosition)
-        		.mapType(MapTypeIdEnum.ROADMAP)
-	            .overviewMapControl(false)
-	            .panControl(false)
-	            .rotateControl(false)
-	            .scaleControl(false)
-	            .streetViewControl(false)
-	            .zoomControl(false)
-	            .zoom(11);
-		
-		googleMap = newGoogleMapView.createMap(defaultMapOptions);
-		
-	}
-	
-	
-	
-	/**
-	 * Retourne l'objet de la carte google maps.
-     * 
+     *
+     * @param newGoogleMapView la vue pour la carte googlemaps.
+     *
+     *
+     * @see MapView#googleMap
+     *
+     */
+    public MapView( GoogleMapView newGoogleMapView){
+
+	MapOptions defaultMapOptions = new MapOptions();
+	// here we set the default location as 47.6097, -122.3331
+	LatLong defaultMapCenterPosition = new LatLong(47.6097, -122.3331);
+
+	defaultMapOptions.center(defaultMapCenterPosition)
+	    .mapType(MapTypeIdEnum.ROADMAP)
+	    .overviewMapControl(false)
+	    .panControl(false)
+	    .rotateControl(false)
+	    .scaleControl(false)
+	    .streetViewControl(false)
+	    .zoomControl(false)
+	    .zoom(11);
+
+	googleMap = newGoogleMapView.createMap(defaultMapOptions);
+
+    }
+
+
+
+    /**
+     * Retourne l'objet de la carte google maps.
+     *
      * @return L'objet map de type GoogleMap.
      */
-	public GoogleMap getGoogleMap(){
-		
-		return googleMap;
-	}
-	
-	/**
-	 * 
-	 * This method waits for the new created PokeMarker to create a Marker on Map.
-	 * PokeMarker is different than Marker. PokeMarker is stored in Map and Marker is an object of the GoogleMap.
-	 * PokeMarker is the model and Marker is the view.
-	 * 	 
-	 * @param pokeMarker
-	 * 					the custom pokemon marker object.					
-	 * 
-	 * @see be.ac.ulb.infof307.g07.Controllers.Handlers.PokeMarkerMouseClickHandler
-	 * @see MapView#googleMap
-	 * 
-	 */
-	public void addMarker(PokeMarker pokeMarker){
-		
-		
-		MarkerOptions markerOption = new MarkerOptions();
-		markerOption.position(new LatLong(pokeMarker.getOnMapPosition().getX(), pokeMarker.getOnMapPosition().getY()));
-		Marker newMarker = new Marker(markerOption);
-		googleMap.addMarker(newMarker);
-		googleMap.addUIEventHandler(newMarker, UIEventType.click, new PokeMarkerMouseClickHandler(newMarker, pokeMarker, googleMap));
-		
-		refreshMap();
-		
-	}
-	
-	/**
-	 * refreshing the map by zooming in and zooming out by 1
-	 * -1 of current is because, when double clicking on map it will automatically zoom by 1, so to keep the same zoom level
-	 * -1 is necessary
-	 * 
-	 * @see MapView#googleMap
-	 */
-	public void refreshMap(){
-		
-		int current = googleMap.getZoom() - 1;
-		googleMap.setZoom(googleMap.getZoom() + 1);
-		googleMap.setZoom(current);
-		
-	}
+    public GoogleMap getGoogleMap(){
+
+	return googleMap;
+    }
+
+    /**
+     *
+     * This method waits for the new created PokeMarker to create a Marker on Map.
+     * PokeMarker is different than Marker. PokeMarker is stored in Map and Marker is an object of the GoogleMap.
+     * PokeMarker is the model and Marker is the view.
+     *
+     * @param pokeMarker
+     * 					the custom pokemon marker object.
+     *
+     * @see be.ac.ulb.infof307.g07.Controllers.Handlers.PokeMarkerMouseClickHandler
+     * @see MapView#googleMap
+     *
+     */
+    public void addMarker(PokeMarker pokeMarker){
+
+
+	MarkerOptions markerOption = new MarkerOptions();
+	markerOption.position(new LatLong(pokeMarker.getOnMapPosition().getX(), pokeMarker.getOnMapPosition().getY()));
+	Marker newMarker = new Marker(markerOption);
+	googleMap.addMarker(newMarker);
+	googleMap.addUIEventHandler(newMarker, UIEventType.click, new PokeMarkerMouseClickHandler(newMarker, pokeMarker, googleMap));
+
+	refreshMap();
+
+    }
+
+    /**
+     * refreshing the map by zooming in and zooming out by 1
+     * -1 of current is because, when double clicking on map it will automatically zoom by 1, so to keep the same zoom level
+     * -1 is necessary
+     *
+     * @see MapView#googleMap
+     */
+    public void refreshMap(){
+
+	int current = googleMap.getZoom() - 1;
+	googleMap.setZoom(googleMap.getZoom() + 1);
+	googleMap.setZoom(current);
+
+    }
 }
