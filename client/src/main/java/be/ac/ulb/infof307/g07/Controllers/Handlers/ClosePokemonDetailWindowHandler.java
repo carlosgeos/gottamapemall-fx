7a4320307e698f14d6c36a8cded1de7fe7cff36a
CFxx <cf1994.live@gmail.com>
2017-04-16 18:08:44 +0200

    Add missing classes

---
+++ client/src/main/java/be/ac/ulb/infof307/g07/Controllers/Handlers/ClosePokemonDetailWindowHandler.java
@@ -0,0 +1,24 @@
+package be.ac.ulb.infof307.g07.Controllers.Handlers;
+
+import be.ac.ulb.infof307.g07.Views.PokedexView;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class ClosePokemonDetailWindowHandler implements EventHandler<ActionEvent>{
+
+	private PokedexView pokedex;
+	
+	public ClosePokemonDetailWindowHandler( PokedexView pokedex ){
+		
+		this.pokedex = pokedex;
+		
+	}
+	
+	@Override
+	public void handle(ActionEvent event) {
+		
+		this.pokedex.closePokemonDetail();
+		
+	}
+
+}
