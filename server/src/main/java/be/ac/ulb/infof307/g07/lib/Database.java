11c529cc7fc2e96f0a879ffff56fb1921e442dd4
Perale Thomas <thomas.perale@openmailbox.org>
2017-04-16 17:50:27 +0200

    [enh] Have the choice to change to select de db name.

--- server/src/main/java/be/ac/ulb/infof307/g07/lib/Database.java
+++ server/src/main/java/be/ac/ulb/infof307/g07/lib/Database.java
@@ -10,12 +10,16 @@ public class Database {
     Database () {}
 
     public static void init () {
+        init("gmta");
+    }
+
+    public static void init (String db) {
         final Morphia morphia = new Morphia();
         morphia.mapPackage("be.ac.ulb.infof307.g07");
 
         try {
             // connect to the database running on localhost with the default port
-            datastore = morphia.createDatastore(new MongoClient(), "gmta");
+            datastore = morphia.createDatastore(new MongoClient(), db);
             datastore.ensureIndexes();
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
