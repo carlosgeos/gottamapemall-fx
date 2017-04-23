5f6317c4bbf9285e7fe898012918117daacd69ef
Perale Thomas <thomas.perale@openmailbox.org>
2017-04-23 14:23:08 +0200

    [fix] Bug fix missing bracket.

--- client/src/main/java/be/ac/ulb/infof307/g07/Views/PokedexView.java
+++ client/src/main/java/be/ac/ulb/infof307/g07/Views/PokedexView.java
@@ -1,6 +1,5 @@
 package be.ac.ulb.infof307.g07.Views;
 
-
 import be.ac.ulb.infof307.g07.Controllers.PokemonViewListener;
 import be.ac.ulb.infof307.g07.Controllers.Handlers.ClosePokemonDetailWindowHandler;
 import be.ac.ulb.infof307.g07.Controllers.Handlers.PokemonViewDblClickHandler;
@@ -57,130 +56,122 @@ public class PokedexView{
 
     public PokedexView( double pokedexViewWidth, double pokedexViewHeight ){
 
-	this.pokedexViewWidth = pokedexViewWidth;
+        this.pokedexViewWidth = pokedexViewWidth;
 
 
-	pokedexBorderPane = new BorderPane();
-	pokemonDetailBorderPane = new BorderPane();
-	pokemonDetailBorderPane.setStyle("-fx-background-color: #FFFFFF");
+        pokedexBorderPane = new BorderPane();
+        pokemonDetailBorderPane = new BorderPane();
+        pokemonDetailBorderPane.setStyle("-fx-background-color: #FFFFFF");
 
-	// Adding TextField on Detail Info Window
-	GridPane newGrid = new GridPane();
-	this.pokemonNameTextField = new TextField("Blank");
-	this.pokemonTypeTextField = new TextField("Blank");
-	this.pokemonHeightField = new Label("Blank");
-	this.pokemonWeightField = new Label("Blak");
-	newGrid.add(new Label("Name: "), 0, 1);
-	newGrid.add(new Label("Type: "), 0, 2);
-	newGrid.add(new Label("Weight (kg): "), 0, 3);
-	newGrid.add(new Label("Height (m): "), 0, 4);
-	newGrid.add(this.pokemonNameTextField, 1, 1);
-	newGrid.add(this.pokemonTypeTextField, 1, 2);
-	newGrid.add(this.pokemonHeightField, 1, 3);
-	newGrid.add(this.pokemonWeightField, 1, 4);
-	// any image only for the purpose of instanciation
-	Image pokemonImage = new Image("http://www.solidbackgrounds.com/images/2560x1440/2560x1440-black-solid-color-background.jpg",this.pokemonImageWidth,this.pokemonImageHeight, true, true);
-	this.pokemonImageView = new ImageView(pokemonImage);
-	newGrid.add(this.pokemonImageView, 0,0);
+        // Adding TextField on Detail Info Window
+        GridPane newGrid = new GridPane();
+        this.pokemonNameTextField = new TextField("Blank");
+        this.pokemonTypeTextField = new TextField("Blank");
+        this.pokemonHeightField = new Label("Blank");
+        this.pokemonWeightField = new Label("Blak");
+        newGrid.add(new Label("Name: "), 0, 1);
+        newGrid.add(new Label("Type: "), 0, 2);
+        newGrid.add(new Label("Weight (kg): "), 0, 3);
+        newGrid.add(new Label("Height (m): "), 0, 4);
+        newGrid.add(this.pokemonNameTextField, 1, 1);
+        newGrid.add(this.pokemonTypeTextField, 1, 2);
+        newGrid.add(this.pokemonHeightField, 1, 3);
+        newGrid.add(this.pokemonWeightField, 1, 4);
+        // any image only for the purpose of instanciation
+        Image pokemonImage = new Image("http://www.solidbackgrounds.com/images/2560x1440/2560x1440-black-solid-color-background.jpg",this.pokemonImageWidth,this.pokemonImageHeight, true, true);
+        this.pokemonImageView = new ImageView(pokemonImage);
+        newGrid.add(this.pokemonImageView, 0,0);
 
-	Button newButton = new Button("Return");
-	newButton.setOnAction(new ClosePokemonDetailWindowHandler(this));
-	newButton.setPrefWidth(this.pokedexViewWidth-16);
+        Button newButton = new Button("Return");
+        newButton.setOnAction(new ClosePokemonDetailWindowHandler(this));
+        newButton.setPrefWidth(this.pokedexViewWidth-16);
 
-	this.pokemonDetailBorderPane.setTop(newButton);
-	BorderPane.setAlignment(newButton, Pos.CENTER_LEFT);
-	this.pokemonDetailBorderPane.setCenter(newGrid);
-	BorderPane.setAlignment(newGrid, Pos.CENTER);
-	this.pokemonDetailBorderPane.setStyle("-fx-background-color:#ffffff");
+        this.pokemonDetailBorderPane.setTop(newButton);
+        BorderPane.setAlignment(newButton, Pos.CENTER_LEFT);
+        this.pokemonDetailBorderPane.setCenter(newGrid);
+        BorderPane.setAlignment(newGrid, Pos.CENTER);
+        this.pokemonDetailBorderPane.setStyle("-fx-background-color:#ffffff");
 
-	this.pokedexStackPane = new StackPane();
+        this.pokedexStackPane = new StackPane();
 
-	this.pokedexStackPane.setMaxSize(pokedexViewWidth, pokedexViewHeight-38);
-	this.pokedexStackPane.setMinSize(pokedexViewWidth, pokedexViewHeight-38);
+        this.pokedexStackPane.setMaxSize(pokedexViewWidth, pokedexViewHeight-38);
+        this.pokedexStackPane.setMinSize(pokedexViewWidth, pokedexViewHeight-38);
 
-	pokedexStackPane.getChildren().addAll(pokemonDetailBorderPane, pokedexBorderPane);
+        pokedexStackPane.getChildren().addAll(pokemonDetailBorderPane, pokedexBorderPane);
 
-	// instance the list for stocking pokemon
-	pokemonInPokedex = FXCollections.observableArrayList();
-	// used only for autofilling the pokedex
-	fillPokedex();
+        // instance the list for stocking pokemon
+        pokemonInPokedex = FXCollections.observableArrayList();
+        // used only for autofilling the pokedex
+        fillPokedex();
 
-	// Vbox will be stocked in ScrollPane
-	pokedexScrollPane = new ScrollPane();
+        // Vbox will be stocked in ScrollPane
+        pokedexScrollPane = new ScrollPane();
 
-	pokedexBorderPane.setCenter(pokedexScrollPane);
+        pokedexBorderPane.setCenter(pokedexScrollPane);
 
-	// it will add all pokemon in vbox
-	updatePokedex();
-
-    }
+        // it will add all pokemon in vbox
+        updatePokedex();
+}
 
     public void updatePokedex(){
+        pokedexScrollPane.setContent(getPokedexViewWithDefaultStyle());
+    }
 
-	pokedexScrollPane.setContent(getPokedexViewWithDefaultStyle());
+    private void fillPokedex(){
+        String response = Requests.get("http://127.0.0.1:4567/pokemons").send().readToText();
+        Gson gson = new Gson();
+        Pokemon[] pokemons = gson.fromJson(response, Pokemon[].class);
 
-	private void fillPokedex(){
-		String response = Requests.get("http://127.0.0.1:4567/pokemons").send().readToText();
-		Gson gson = new Gson();
-		Pokemon[] pokemons = gson.fromJson(response, Pokemon[].class);
-
-		for (int i = 0; i < pokemons.length; ++i) {
-			pokemonInPokedex.add(pokemons[i]);
-		}
+        for (int i = 0; i < pokemons.length; ++i) {
+            pokemonInPokedex.add(pokemons[i]);
+        }
     }
 
     public VBox getPokedexViewWithDefaultStyle(){
-
-	PokemonViewDblClickHandler newHandler = new PokemonViewDblClickHandler(this);
-	return getPokedexView( newHandler, this.pokedexViewWidth-15, 50, new Insets(5,5,5,5), this.pokemonIconWidth, this.pokemonIconHeight, 5, 10 );
-
+        PokemonViewDblClickHandler newHandler = new PokemonViewDblClickHandler(this);
+        return getPokedexView( newHandler, this.pokedexViewWidth-15, 50, new Insets(5,5,5,5), this.pokemonIconWidth, this.pokemonIconHeight, 5, 10 );
     }
 
     public VBox getPokedexView(PokemonViewListener handler, double pokemonViewWidth, double pokemonViewHeight, Insets pokemonPadding, double iconWidth, double iconHeight, double VGap, double HGap){
 
-	VBox newVBox = new VBox();
+        VBox newVBox = new VBox();
 
-	for(int i = 0; i < this.pokemonInPokedex.size(); ++i){
+        for(int i = 0; i < this.pokemonInPokedex.size(); ++i) {
+            PokemonView newPokemonView = new PokemonView(this.pokemonInPokedex.get(i), this);
+            newPokemonView.registerListener(handler);
 
-	    PokemonView newPokemonView = new PokemonView(this.pokemonInPokedex.get(i), this);
-	    newPokemonView.registerListener(handler);
+            newVBox.getChildren().add(newPokemonView.createView( pokemonViewWidth, pokemonViewHeight, pokemonPadding, iconWidth, iconHeight, VGap, HGap ));
+        }
 
-	    newVBox.getChildren().add(newPokemonView.createView( pokemonViewWidth, pokemonViewHeight, pokemonPadding, iconWidth, iconHeight, VGap, HGap ));
-	}
-
-	return newVBox;
-
+        return newVBox;
     }
 
     public StackPane getView(){
-	return pokedexStackPane;
+        return pokedexStackPane;
     }
 
-    public void showPokemonDetail( Pokemon pokemon )
-	{
-	    // prepare all info of this pokemon
-	    this.pokemonNameTextField.setText(pokemon.getName());
-	    this.pokemonTypeTextField.setText(pokemon.getType());
-	    this.pokemonHeightField.setText(Double.toString(pokemon.getHeight()));
-	    this.pokemonWeightField.setText(Double.toString(pokemon.getWeight()));
-	    Image pokemonImage = new Image(pokemon.getImagePath(), 100, 130, true, true);
-	    this.pokemonImageView.setImage(pokemonImage);
+    public void showPokemonDetail( Pokemon pokemon ) {
+        // prepare all info of this pokemon
+        this.pokemonNameTextField.setText(pokemon.getName());
+        this.pokemonTypeTextField.setText(pokemon.getType());
+        this.pokemonHeightField.setText(Double.toString(pokemon.getHeight()));
+        this.pokemonWeightField.setText(Double.toString(pokemon.getWeight()));
+        Image pokemonImage = new Image(pokemon.getImagePath(), 100, 130, true, true);
+        this.pokemonImageView.setImage(pokemonImage);
 
-	    // send the window of detail info to the front
-	    // and others to the back
-	    this.pokemonDetailBorderPane.toFront();
-	    // set the visibility of them too
-	    this.pokemonDetailBorderPane.setOpacity(1);
-	    this.pokedexBorderPane.setOpacity(0);
+        // send the window of detail info to the front
+        // and others to the back
+        this.pokemonDetailBorderPane.toFront();
+        // set the visibility of them too
+        this.pokemonDetailBorderPane.setOpacity(1);
+        this.pokedexBorderPane.setOpacity(0);
 
-	}
+    }
 
     public void closePokemonDetail(){
-
-	// just do the opposite of showPokemonDetail
-	this.pokedexBorderPane.toFront();
-	this.pokedexBorderPane.setOpacity(1);
-	this.pokemonDetailBorderPane.setOpacity(0);
-
+        // just do the opposite of showPokemonDetail
+        this.pokedexBorderPane.toFront();
+        this.pokedexBorderPane.setOpacity(1);
+        this.pokemonDetailBorderPane.setOpacity(0);
     }
 }
