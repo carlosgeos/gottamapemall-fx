62f69f20badd7ddd3ebb3af1d1a27637dcf48e95
Perale Thomas <thomas.perale@openmailbox.org>
2017-05-04 18:36:45 +0200

    [fix] Handler creation.

--- client/src/main/java/be/ac/ulb/infof307/g07/controllers/Handlers/FilterSaveHandler.java
+++ client/src/main/java/be/ac/ulb/infof307/g07/controllers/Handlers/FilterSaveHandler.java
@@ -1,35 +1,21 @@
 package be.ac.ulb.infof307.g07.controllers.Handlers;
 
+import be.ac.ulb.infof307.g07.views.FilterView;
+import be.ac.ulb.infof307.g07.models.FilterModel;
+import net.dongliu.requests.Requests;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
 public class FilterSaveHandler implements EventHandler<ActionEvent>{
+    private FilterView view;
 
-	private String critName;
-	private String critType1;
-	private String critType2;
-	private int critHeight;
-	private int critWeight;
-	private int critBaseExp;
-	private String filterName;
-	
-	public FilterSaveHandler( String name, String type1, String type2, int height, int weight, int exp, String filterName){
-		
-		this.critName = name;
-		this.critType1 = type1;
-		this.critType2 = type2;
-		this.critHeight = height;
-		this.critWeight = weight;
-		this.critBaseExp = exp;
-		this.filterName = filterName;
-	}
-	
-	@Override
-	public void handle(ActionEvent event) {
-		
-		// save to server
-		
-		
-	}
+    public FilterSaveHandler(FilterView view) {
+        this.view = view;    
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        FilterModel filter = view.getFilter();
+    }
 
 }
--- client/src/main/java/be/ac/ulb/infof307/g07/controllers/Handlers/FilterSearchHandler.java
+++ client/src/main/java/be/ac/ulb/infof307/g07/controllers/Handlers/FilterSearchHandler.java
@@ -1,34 +1,18 @@
 package be.ac.ulb.infof307.g07.controllers.Handlers;
 
+import be.ac.ulb.infof307.g07.views.FilterView;
+import net.dongliu.requests.Requests;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
-public class FilterSearchHandler implements EventHandler<ActionEvent>{
-
-	
-	private String critName;
-	private String critType1;
-	private String critType2;
-	private int critHeight;
-	private int critWeight;
-	private int critBaseExp;
-	
-	public FilterSearchHandler( String name, String type1, String type2, int height, int weight, int exp){
-		
-		this.critName = name;
-		this.critType1 = type1;
-		this.critType2 = type2;
-		this.critHeight = height;
-		this.critWeight = weight;
-		this.critBaseExp = exp;
-		
-	}
-	
-	@Override
-	public void handle(ActionEvent event) {
-		
-		// send to server and update the mapview
-		
-	}
-
+public class FilterSearchHandler implements EventHandler<ActionEvent> {
+    private FilterView view;
+    
+    public FilterSearchHandler(FilterView view) {
+        this.view = view;
+    }
+    
+    @Override
+    public void handle(ActionEvent event) {
+    }
 }
