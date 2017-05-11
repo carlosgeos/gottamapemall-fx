ad94c784ed1a16f5aee961fa8c7ca2ce981ccbfc
Perale Thomas <thomas.perale@openmailbox.org>
2017-05-11 13:46:12 +0200

    [add] FilterView

--- server/src/main/java/be/ac/ulb/infof307/g07/Main.java
+++ server/src/main/java/be/ac/ulb/infof307/g07/Main.java
@@ -3,6 +3,7 @@ package be.ac.ulb.infof307.g07;
 import be.ac.ulb.infof307.g07.libs.Database;
 import be.ac.ulb.infof307.g07.views.PokemonView;
 import be.ac.ulb.infof307.g07.views.LocationView;
+import be.ac.ulb.infof307.g07.views.FilterView;
 
 public class Main {
     public static void main(final String[] args) {
@@ -10,5 +11,6 @@ public class Main {
 
         new PokemonView();
         new LocationView();
+        new FilterView();
     }
 }
