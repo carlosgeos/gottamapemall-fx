b3fffe29a526b56b50ee6f68d6a6ff9c45ed014e
Perale Thomas <thomas.perale@openmailbox.org>
2017-05-11 13:09:41 +0200

    [fix] Comments to talk about the abstraction of the classes Message and Error.

--- server/src/main/java/be/ac/ulb/infof307/g07/libs/Error.java
+++ server/src/main/java/be/ac/ulb/infof307/g07/libs/Error.java
@@ -1,6 +1,17 @@
 package be.ac.ulb.infof307.g07.libs;
 
+/**
+ * La classe "Error" sert à abstraire un message d'erreur qui va être renvoyé 
+ * aux utilisateur de l'API, elle ne stocke qu'un message en son sein qui est
+ * là pour seul but d'être serialisé en json.
+ *
+ * @see be.ac.ulb.infof307.g07.libs.Message
+ * @see com.google.gson.Gson
+ */
 public class Error {
+    /**
+     * Stocke le message d'erreur affiché aux utilisateurs de l'API.
+     */
     String error;
 
     public Error (String error) {
